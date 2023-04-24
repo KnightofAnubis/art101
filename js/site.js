bf125b6917d087bae35bdae8987174d640f1f703
Kelsey Melott <knightsofanubis352@gmail.com>
2023-04-24 10:59:03 -0700

    lab 4

--- js/site.js
+++ js/site.js
@@ -5,6 +5,21 @@
 // Constants
 
 // Functions
+//slideshow from W3Schools
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000);
+}
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Dots and Boxes
+//An implementation of the game
+// Author: Kelsey Melott
+// Date: 4/24/2023
 
 // Constants
 
@@ -18,4 +19,23 @@ function main() {
 }
 
 // let's get this party started
+
+//print updated board before each turn
+	//loop through each row
+		//loop through each space
+			//Print either dot, line - between the dots, or space between dots
+//Ask user where they want to place their line
+//place line
+//if line completes a box 
+	//color box to signify it is player’s
+//evaluate possible moves left
+	//loop through each row to see where there are lines vs. spaces
+//the computer makes a move
+	//draws line in an available space
+//returns board for player’s move
+//when there are no spaces left
+	//count number of boxes for player and computer
+	//print who won
+		//player with most boxes
+
 main();
